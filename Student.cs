3f3da69a058bd554c066a555f10599ca28afb876
ducanh2004 <110523685+ducanh2004@users.noreply.github.com>
2023-04-14 14:02:45 +0700

    baitap1

---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    internal class Student
+    {
+        public string name;
+        public string email;
+        public int age;
+        public string telephone;
+        public int tu;
+        public int mau;
+
+        public Student() 
+        {
+            this.run();
+            this.learn();
+        }
+
+        public void run()
+        {
+            Console.WriteLine(this.name + "Running...");
+        }
+        public void learn()
+        {
+            Console.WriteLine(this.name + "Learning...");
+        }
+        
+
+    }
+    
+
+}
