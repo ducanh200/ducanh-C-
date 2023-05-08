f894a98783655ef7afcd6cfbe3e2689f30260792
ducanh2004 <ducanh2004@users.noreply.github.com>
2023-05-08 14:54:38 +0700

    baithi

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using T2207A;
+class Program
+{
+    static List<Product> products = new List<Product>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add product ");
+            Console.WriteLine("2. Display product");
+            Console.WriteLine("3. Delete product by Id");
+            Console.WriteLine("4. Exit");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    AddProduct();
+                    break;
+                case 2:
+                    DisplayProducts();
+                    break;
+                case 3:
+                    DeleteProduct();
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    static void AddProduct()
+    {
+        Console.Write("Enter product ID: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter product name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter product price: ");
+        double price = double.Parse(Console.ReadLine());
+
+        Product product = new Product(id, name, price);
+        products.Add(product);
+
+        Console.WriteLine("Product added .");
+    }
+
+    static void DisplayProducts()
+    {
+        if (products.Count == 0)
+        {
+            Console.WriteLine("No products found.");
+        }
+        else
+        {
+            Console.WriteLine("Product records:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+
+    static void DeleteProduct()
+    {
+        Console.Write("Enter product ID to delete: ");
+        int id = int.Parse(Console.ReadLine());
+
+        bool found = false;
+        foreach (Product product in products)
+        {
+            if (product.Id == id)
+            {
+                products.Remove(product);
+                Console.WriteLine("Product deleted.");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Product not found.");
+        }
+    }
+}
---
+++ QuanLySanPham.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Price: {Price}";
+        }
+    }
+}
